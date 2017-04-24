c7d5403630b761cd112f55fe07f55cbbd9cc46af
Jianfei2333 <xjf999999@hotmail.com>
2017-04-24 21:41:19 +0800

    中大图书馆超期做题自动脚本

---
+++ auto_exam_lib.py
@@ -0,0 +1,51 @@
+#coding: UTF-8
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+browser.get('http://202.116.65.85/sysulib/')
+cardNo = browser.find_element_by_id("cardNo")
+
+#Id = raw_input('请输入学生证号:')
+Id = "15336204"
+
+cardNo.send_keys(Id)
+submit = browser.find_element_by_id("submit1").click()
+
+wait1 = WebDriverWait(browser, 10).until(
+        EC.invisibility_of_element_located((By.XPATH, "//div[@id = 'LoadingDiv']"))
+)
+
+status = browser.find_element_by_id("QuestionTitle").get_attribute("alt")
+if status == "错误":
+    browser.quit()
+
+wait2 = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//input[@id = 'answer' and @value = '1']"))
+)
+total = browser.find_element_by_id("totalCount").get_attribute("value")
+
+#print(total)
+
+for i in range(0, int(total)):
+    rightAnswer = browser.find_element_by_id("rightAnswer").get_attribute("value")
+    print(rightAnswer)
+    if rightAnswer == '1': 
+        choose = browser.find_element_by_xpath("//input[@name = 'answer'][@value = '1']").click()
+    else:
+        choose = browser.find_element_by_xpath("//input[@name = 'answer'][@value = '2']").click()
+        
+    browser.find_element_by_id("submit2").click()
+    print("done:", i)
+    browser.implicitly_wait(3)
+    wait = WebDriverWait(browser, 10).until(
+        EC.invisibility_of_element_located((By.XPATH, "//div[@id = 'loadingDiv2']"))
+    )
+
+
+
+browser.quit()
+
